0abee374daf2ce32431aeb9814347269a31fbdcb
Nick <nickmeares@gmail.com>
2022-02-02 21:59:16 +0100

    February 2, 2022 9:59 PM

--- setup.py
+++ setup.py
@@ -7,11 +7,18 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+# Dependencies
+requirements = []
+with open('requirements.txt', 'r') as fh:
+    for line in fh:
+        requirements.append(line.strip())
+
 # This call to setup() does all the work
 setup(
     name="stockmarketmba",
     version="1.0.2",
     long_description=README,
     long_description_content_type="text/markdown",
-    packages=["stockmarketmba"]
+    packages=["stockmarketmba"], 
+    install_requires = requirements
 )
