56bcef45b3547c8e3966bf9088488ec5f29bc751
Nick <nickmeares@gmail.com>
2022-02-02 17:37:19 +0100

    February 2, 2022 5:37 PM

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="StockMarketMBA",
+    name="stockmarketmba",
     version="1.0",
     long_description=README,
     long_description_content_type="text/markdown",
