ff5cd0717948b5f5d4af5bd1295f8b83e781e879
Nick <nickmeares@gmail.com>
2022-02-02 21:40:15 +0100

    February 2, 2022 9:40 PM

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from StockMarketMBA import stockmarketmba as mba
+from ..StockMarketMBA import stockmarketmba as mba
 
 api = mba.api()
 
